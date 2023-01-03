6d18d0be53575b363c3231e889c11afbc5024ebe
bilwaf <bilawafaa09@gmail.com>
2023-01-03 15:00:54 +0700

    Validasi Input

---
+++ CRUDMahasiswaSederhana/src/main/java/com/mycompany/crudmahasiswasederhana/CRUDMahasiswaSederhana.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.crudmahasiswasederhana;
+
+import crudmahasiswasederhana.views.MahasiswaFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author ASUS ZenBook
+ * NABILA WAFAA' YULISTYANINGRUM
+ * 21103082
+ * S1SI05C
+ */
+
+public class CRUDMahasiswaSederhana {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            public void run(){
+                MahasiswaFrame form = new MahasiswaFrame();
+                form.setLocationRelativeTo(null);
+                form.setVisible(true);
+            }
+        });
+    }
+}
---
+++ CRUDMahasiswaSederhana/src/main/java/crudmahasiswasederhana/interfc/MahasiswaInterface.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package crudmahasiswasederhana.interfc;
+
+import crudmahasiswasederhana.models.Mahasiswa;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS ZenBook
+ * NABILA WAFAA' YULISTYANINGRUM
+ * 21103082
+ * S1SI05C
+ */
+
+public interface MahasiswaInterface {
+    Mahasiswa insert (Mahasiswa o) throws SQLException;
+    
+    void update (Mahasiswa o) throws SQLException;
+    
+    void delete (String nim) throws SQLException;
+    
+    List<Mahasiswa> getAll() throws SQLException;
+}
